94843372c3a3ca7168c27b4cf17884831471bf80
SpiderWolf6 <sohamhomework@gmail.com>
2024-08-07 16:05:32 -0400

    added new rows

--- neoantigen_pipeline.py
+++ neoantigen_pipeline.py
@@ -1,12 +1,15 @@
 import io
 import os
 import sys
+import time
+
 import pandas as pd
 import vcf
 import vcf.parser
 import argparse
 import requests
 import json
+import numpy as np
 
 # initialize user input parser and arguments
 parser = argparse.ArgumentParser(description='Foo')
@@ -68,51 +71,74 @@ def traverse_tree(tree, sample):
         missense_counter = 0
         frameshift_counter = 0
         mutations_counter = 0
+        clone_neoantigens = 0
+        clone_weakBinders = 0
+        clone_strongBinders = 0
 
-        for m in item["clone_mutations"]:
+        for mutation_id in item["clone_mutations"]:
             mutations_counter += 1
-            mut_type = (vcf_data.loc[vcf_data.index[vcf_data['ID'] == m], 'ANN'].iloc[0])[0]
-            start = mut_type.find("|") + 1
-            end = mut_type[start:].find("|") + start
-            mut_type = mut_type[start:end]
+            mutation_type = (vcf_data.loc[vcf_data.index[vcf_data['ID'] == mutation_id], 'ANN'].iloc[0])[0]
+            start = mutation_type.find("|") + 1
+            end = mutation_type[start:].find("|") + start
+            mutation_type = mutation_type[start:end]
 
-            if mut_type.startswith("missense"):
+            if mutation_type.startswith("missense"):
                 missense_counter += 1
-            if mut_type.startswith("frameshift"):
+            if mutation_type.startswith("frameshift"):
                 frameshift_counter += 1
 
-            sub_df = neoantigen_data[neoantigen_data['neoantigen'].str.startswith(m)]
-            if len(sub_df) > 0:
-                binders = len(sub_df)
-                gene = sub_df['gene'].values[0]
-                kdmt_min = sub_df.min(axis=0)['kDmt']
+            sub_mutation_df = neoantigen_data[neoantigen_data['neoantigen'].str.startswith(mutation_id)]
+            if len(sub_mutation_df) > 0:
+                binders = len(sub_mutation_df)
+                gene = sub_mutation_df['gene'].values[0]
+                mutation_kDmt = sub_mutation_df.min(axis=0)['kDmt']
+                mutation_strongBinders = len(sub_mutation_df[sub_mutation_df["kDmt"] < 50])
+                mutation_weakBinders = len(sub_mutation_df[(sub_mutation_df["kDmt"] < 500)]) - mutation_strongBinders
 
-                mutation_summary = pd.concat([mutation_summary, pd.DataFrame({"Sample": [sample], "Mutation_ID": [m],
+                clone_neoantigens += binders
+                clone_strongBinders += mutation_strongBinders
+                clone_weakBinders += mutation_weakBinders
+
+                mutation_summary = pd.concat([mutation_summary, pd.DataFrame({"Sample": [sample], "Mutation_ID": [mutation_id],
                                                                               "clone_id": item['clone_id'],
                                                                               "CCF_X": item['X'],
                                                                               "marginalCCF_x": item['x'],
-                                                                              "mutation_type": mut_type, "gene": gene,
+                                                                              "mutation_type": mutation_type, "gene": gene,
                                                                               "n_neoantigen_binders": binders,
-                                                                              "best_kDmt": kdmt_min})],
+                                                                              "best_kDmt": mutation_kDmt,
+                                                                              "n_weakBinders": mutation_weakBinders,
+                                                                              "n_strongBinders": mutation_strongBinders})],
                                              ignore_index=True)
 
             else:
-                mutation_summary = pd.concat([mutation_summary, pd.DataFrame({"Sample": [sample], "Mutation_ID": [m],
+                mutation_summary = pd.concat([mutation_summary, pd.DataFrame({"Sample": [sample], "Mutation_ID": [mutation_id],
                                                                               "clone_id": item['clone_id'],
                                                                               "CCF_X": item['X'],
                                                                               "marginalCCF_x": item['x'],
-                                                                              "mutation_type": mut_type, "gene": None,
+                                                                              "mutation_type": mutation_type, "gene": None,
                                                                               "n_neoantigen_binders": 0,
-                                                                              "best_kDmt": 0})],
+                                                                              "best_kDmt": 0,
+                                                                              "n_weakBinders": 0,
+                                                                              "n_strongBinders": 0})],
                                              ignore_index=True)
 
+        sub_clonal_df = neoantigen_data[neoantigen_data['clone_number'] == item['clone_id']]
+        quality = sub_clonal_df.max(axis=0)['quality']
+        fitness = sub_clonal_df.min(axis=0)['clone_fitness']
+        clone_kDmt = sub_clonal_df.min(axis=0)['kDmt']
         clone_summary = pd.concat([clone_summary, pd.DataFrame({"Sample": [sample], "Clone_ID": [item['clone_id']],
                                                                 "CCF_X": item['X'], "marginalCCF_x": item['x'],
                                                                 "n_mutations": mutations_counter,
                                                                 "n_missense": missense_counter,
                                                                 "n_frameshift": frameshift_counter,
                                                                 "parent_clone_id": parent_list[-2],
-                                                                "all_parent_clones": ','.join(str(x) for x in parent_list[:-1])})],
+                                                                "all_parent_clones": ','.join(str(x) for x in parent_list[:-1]),
+                                                                "n_neoantigen_binders": clone_neoantigens,
+                                                                "n_weakBinders": clone_weakBinders,
+                                                                "n_strongBinders": clone_strongBinders,
+                                                                "clone_fitness": fitness,
+                                                                "best_quality": quality,
+                                                                "best_kDmt": clone_kDmt})],
                                   ignore_index=True)
 
         if 'children' in item:
@@ -126,8 +152,8 @@ def traverse_tree(tree, sample):
 def tree_summary(path):
     os.chdir(path)
     print("entered")
-    for p_name in patient_summary.Patient.unique():
-        tfile = "trees_" + p_name + ".json"
+    if patient:
+        tfile = "trees_" + patient + ".json"
         f = open(tfile)
         data = json.load(f)
         original_tree = data['time_points'][0]['samples']
@@ -136,6 +162,17 @@ def tree_summary(path):
             tree_dict[x['id']] = x['sample_trees'][0]['topology']
             traverse_tree(x['sample_trees'][0]['topology'], x['id'])
             print("\n")
+    else:
+        for p_name in patient_summary.Patient.unique():
+            tfile = "trees_" + p_name + ".json"
+            f = open(tfile)
+            data = json.load(f)
+            original_tree = data['time_points'][0]['samples']
+            for x in original_tree:
+                print(x['id'])
+                tree_dict[x['id']] = x['sample_trees'][0]['topology']
+                traverse_tree(x['sample_trees'][0]['topology'], x['id'])
+                print("\n")
 
 
 # add total mutations and total non-synonymous mutations columns to patient summary file
@@ -157,40 +194,46 @@ def total_cols():
         col_list.remove(x)
     patient_summary['total_mutations'] = patient_summary[col_list].sum(axis=1)
 
-
-# for each patient, open respective neo antigen file and read into dataframe
 # save neo antigen dataframe in neoantigen_dict with sample as key
 # count binders and collect data to add to patient summary file
+def neoantigen_data(data, nf_filename):
+    unique_9mers = []
+    unique_binders = []
+    weakbind_count = 0
+    strongbind_count = 0
+
+    for index, row in data.iterrows():
+        if row['peptideMT'] not in unique_9mers:
+            unique_9mers.append(row['peptideMT'])
+        if row['neoantigen'] not in unique_binders:
+            unique_binders.append(row['neoantigen'])
+        if row['kDmt'] < 500:
+            if row['kDmt'] > 50:
+                weakbind_count += 1
+            else:
+                strongbind_count += 1
+
+    for y in patient_summary['Sample']:
+        if y in nf_filename:
+            neoantigen_dict[y] = data
+            i = patient_summary.index[patient_summary['Sample'] == y]
+            patient_summary.loc[i, ["n_unique_9mers", "n_neoantigen_binders", "n_weakBinders",
+                                    "n_strongBinders"]] = [len(unique_9mers), len(unique_binders),
+                                                                weakbind_count, strongbind_count]
+
+# for each patient, open respective neo antigen file and read into dataframe
 def neoantigen_summary(path):
     for dirpath, dirname, filename in os.walk(path):
         for f in filename:
             if f.startswith("nf_"):
-                unique_9mers = []
-                unique_binders = []
-                weakbind_count = 0
-                strongbind_count = 0
-
-                data = pd.read_csv(os.path.join(path, f), sep='\t', header=0)
-                for index, row in data.iterrows():
-                    if row['peptideMT'] not in unique_9mers:
-                        unique_9mers.append(row['peptideMT'])
-                    if row['neoantigen'] not in unique_binders:
-                        unique_binders.append(row['neoantigen'])
-                    if row['kDmt'] < 500:
-                        if row['kDmt'] > 50:
-                            weakbind_count += 1
-                        else:
-                            strongbind_count += 1
-
-                for y in patient_summary['Sample']:
-                    if y in f:
-                        neoantigen_dict[y] = data
-                        i = patient_summary.index[patient_summary['Sample'] == y]
-                        patient_summary.loc[i, ["n_unique_9mers", "n_binders_9mers", "n_weakBinders_9mer",
-                                                "n_strongBinders_9mer"]] = [len(unique_9mers), len(unique_binders),
-                                                                            weakbind_count, strongbind_count]
+                if patient:
+                    if patient in f:
+                        data = pd.read_csv(os.path.join(path, f), sep='\t', header=0)
+                        neoantigen_data(data, f)
+                else:
+                    data = pd.read_csv(os.path.join(path, f), sep='\t', header=0)
+                    neoantigen_data(data, f)
     total_cols()
-
 
 # count unique mutations and feature types, add to patient summary file
 def count(sample):
@@ -239,9 +282,13 @@ def findpath(path):
     os.chdir(path)
     if os.path.isdir("VCF"):
         os.chdir(path + "/VCF")
-        for sub_dir in os.listdir():
-            new_path = os.path.join(os.getcwd(), sub_dir)
-            search_dir(new_path, sub_dir)
+        if patient:
+            new_path = os.path.join(os.getcwd(), patient)
+            search_dir(new_path, patient)
+        else:
+            for sub_dir in os.listdir():
+                new_path = os.path.join(os.getcwd(), sub_dir)
+                search_dir(new_path, sub_dir)
     else:
         return "VCF not found in directory"
 
@@ -251,13 +298,15 @@ findpath(vcf_directory)
 neoantigen_summary(neoant_directory)
 tree_summary(tree_directory)
 
-# If User specified patient, update summary files to contain only patient-specific data
+# If User specified patient, check that summary files only contain patient-specific data
 if patient:
     patient_summary = patient_summary[patient_summary['Patient'] == patient]
     mutation_summary = mutation_summary[mutation_summary['Sample'].str.contains(patient)]
     clone_summary = clone_summary[clone_summary['Sample'].str.contains(patient)]
 
-# export summary files as .csv
-patient_summary.to_csv()
+mutation_summary['best_kDmt'].replace(0, np.nan, inplace=True)
+
 mutation_summary.to_csv()
 clone_summary.to_csv()
+patient_summary.to_csv()
+# export summary files as .csv
